ce952225b52ce7b31ff7403ed61a3186b43827a3
pya-py <p4ya.py@gmail.com>
2022-05-11 07:46:05 +0430

    Updates

---
+++ precache-manifest.864ceb4406213cf62dd5f5b4771b1f8a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6be894b9bf89b9f4ddd06b593a7c65b",
+    "url": "/polsazeh/index.html"
+  },
+  {
+    "revision": "3b09ba1d72a5754d8777",
+    "url": "/polsazeh/static/css/main.321e6e2f.chunk.css"
+  },
+  {
+    "revision": "50c10be408a1409dccf6",
+    "url": "/polsazeh/static/js/2.4a65c416.chunk.js"
+  },
+  {
+    "revision": "1bde7cbddc37293b60694441b834365b",
+    "url": "/polsazeh/static/js/2.4a65c416.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b09ba1d72a5754d8777",
+    "url": "/polsazeh/static/js/main.dbbd4395.chunk.js"
+  },
+  {
+    "revision": "02fc21201ef4f6cac6d4",
+    "url": "/polsazeh/static/js/runtime-main.320273dd.js"
+  },
+  {
+    "revision": "6ed3dff1a39f74a75b629260de9f3f9a",
+    "url": "/polsazeh/static/media/test.6ed3dff1.jpg"
+  }
+]);
